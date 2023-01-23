98afd709d1ba2a488d68765b224dfa8185d04790
bystrovdev <bystrovdev@mail.ru>
2023-01-23 17:52:12 +0400

    Added asyncTask example xml

--- app/src/main/java/ru/synergy/asyncexample/AsyncTaskExample.java
+++ app/src/main/java/ru/synergy/asyncexample/AsyncTaskExample.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 
 public class AsyncTaskExample extends AppCompatActivity {
 
@@ -14,6 +15,10 @@ public class AsyncTaskExample extends AppCompatActivity {
 
         MyAsyncTask asyncTask = new MyAsyncTask();
         asyncTask.execute("Hello world");
+    }
+
+    public void onClick(View v) {
+        //TODO
     }
 }
 
